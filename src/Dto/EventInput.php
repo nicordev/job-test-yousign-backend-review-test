343e66ccc9f648115bfe24624ce148e656561f2f
nicordev <nicolas.renvoise.dev@gmail.com>
2024-02-09 14:28:03 +0100

    remove constructor, add type hints

--- src/Dto/EventInput.php
+++ src/Dto/EventInput.php
@@ -10,9 +10,4 @@ class EventInput
      * @Assert\Length(min=20)
      */
     public ?string $comment;
-
-    public function __construct(?string $comment)
-    {
-        $this->comment = $comment;
-    }
 }
